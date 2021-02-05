db0f876dde9e2bbe7a666128bf67394467d36aae
Andrew Nieuwsma <andrew.nieuwsma@hpe.com>
2021-02-05 07:49:40 -0600

    updated vendor

--- vendor/stash.us.cray.com/HMS/hms-dns-dhcp/pkg/dns_dhcp.go
+++ vendor/stash.us.cray.com/HMS/hms-dns-dhcp/pkg/dns_dhcp.go
@@ -1,15 +1,37 @@
-// Copyright 2019 Cray Inc.
+// MIT License
+//
+// (C) Copyright [2019, 2021] Hewlett Packard Enterprise Development LP
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
 
 package dns_dhcp
 
 import (
-    "bytes"
-    "encoding/json"
-    "fmt"
-    "github.com/hashicorp/go-retryablehttp"
-    "io/ioutil"
-    "net/http"
-    "stash.us.cray.com/HMS/hms-smd/pkg/sm"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/hashicorp/go-retryablehttp"
+	"stash.us.cray.com/HMS/hms-smd/pkg/sm"
 )
 
 type DNSDHCPHelper struct {
@@ -120,7 +142,8 @@ func (helper *DNSDHCPHelper) PatchEthernetInterface(theInterface sm.CompEthInter
         return
     }
 
-    url := fmt.Sprintf("%s/Inventory/EthernetInterfaces/%s", helper.HSMURL, theInterface.MACAddr)
+    macID := strings.ReplaceAll(theInterface.MACAddr, ":", "")
+    url := fmt.Sprintf("%s/Inventory/EthernetInterfaces/%s", helper.HSMURL, macID)
 
     request, requestErr := retryablehttp.NewRequest("PATCH", url, bytes.NewBuffer(payloadBytes))
     if requestErr != nil {
