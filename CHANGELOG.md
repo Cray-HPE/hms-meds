a4455c9c8a543874d82ef398e5b4d9cf5837be30
Steven Presser <spresser@cray.com>
2021-02-09 19:05:15 -0500

    Merge release/shasta-1.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,11 +4,24 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
-## [1.14.0] - 2021-2-09
+
+## [1.11.9] - 2021-2-09
 
 ### Changed
 
 - MEDS no longer expects specific IP addresses for nodes and instead looks them up using DNS.
+
+## [1.11.8] - 2021-02-08
+
+### Changed
+
+- Update Copyright/License info
+
+## [1.11.7] - 2021-01-22
+
+### Changed
+
+- CASMHMS-4367 - MEDS no longer marks redfishEndpoints as disabled in HSM when they are no longer on the network, as this is not necessary.
 
 ## [1.11.6] - 2021-01-19
 
--- cmd/meds/meds.go
+++ cmd/meds/meds.go
@@ -1,4 +1,24 @@
-// Copyright 2018-2020 Hewlett Packard Enterprise Development LP
+// MIT License
+//
+// (C) Copyright [2018-2021] Hewlett Packard Enterprise Development LP
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
 
 package main
 
@@ -473,7 +493,9 @@ func notifyHSMXnameNotPresent(node NetEndpoint) *error {
 	//return nil
 	//log.Printf("DEBUG: DELETE to %s/Inventory/RedfishEndpoints/%s", hsm, node.name)
 
-	return patchXName(node.name, false)
+	log.Printf("DEBUG: Would remove %s, but MEDS no longer marks redfishEndpoints as disabled. This message is purely for your information; MEDS is operating as expected.", node.name)
+	//return patchXName(node.name, false)
+	return nil
 }
 
 func queryHSMState() error {
